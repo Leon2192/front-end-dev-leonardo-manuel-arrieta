b3ae32a850b630969660c047f9131116d6e41eda
Leonardo <leon.strokes@gmail.com>
2022-08-15 20:18:54 -0300

    About

--- src/components/PortfolioComponent/About/About.js
+++ src/components/PortfolioComponent/About/About.js
@@ -11,13 +11,14 @@ const About = () => {
           persona entusiasta, organizada, muy detallista y con facilidad para
           relacionarme con las personas. Me apasiona programar y mi objetivo es
           desarrollarme profesionalmente para poder dedicarme a ello. Hace 4
-          años que me formo profesionalmente como front-end developer realiando
+          años me capacito para ser front-end developer realizando
           cursos en plataformas como Coderhouse, UTN E-learning, Educación IT,
-          entre otros. Las tecnologías que habitualmente uso son HTML, CSS,
-          Javascript, React Js, React Native, Expo, Next Js, Angular Js y Node
-          Js. Actualmente busco poner en práctica todos mis conocimientos en un
-          entorno profesional y desafiante para poder insertarme en el área IT y
-          desempeñarme como Front-end Developer.
+          entre otros. Hoy me desenvuelvo como Tutor del framework React Js en
+          un coding bootcamp (Coderhouse). Las tecnologías que habitualmente uso
+          son HTML, CSS, Javascript, React Js, React Native, Expo, Next Js,
+          Angular Js y Node Js. Actualmente busco poner en práctica todos mis
+          conocimientos en un entorno profesional y desafiante para poder
+          insertarme en el área IT y desempeñarme como Front-end Developer.
         </p>
       </div>
     </div>
